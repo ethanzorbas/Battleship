cb8bc3657572af2831f0e70b16ac048c78c85764
enzor <enzor@host.docker.internal>
2022-04-14 15:01:31 -0700

    Server connection is now setup. Server must play first by running program with command line argument of "server" and then run another instance with the argument of "client" to begin the application

--- Battleship/src/application/Battleship.java
+++ Battleship/src/application/Battleship.java
@@ -5,26 +5,61 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 
 public class Battleship extends Application {
+	
+	/* Constants for the scene */
+	private static final int SCENE_HEIGHT = 800;
+	private static final int SCENE_WIDTH = 1200;
+	
+	private static String user;
+	
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			
+			BattleshipController controller = new BattleshipController(user);
+			
+			stage.setTitle("Battleship");
+			
+			Button testButton = new Button("testing");
+			Button testButton2 = new Button("testing2");
+			Text titleText = new Text("Battleship");
+			Pane playerBoard = new Pane(testButton);
+			Pane enemyBoard = new Pane(testButton2);
+			
+			HBox hbox = new HBox(testButton, testButton2);
+			hbox.setSpacing(50);
+			hbox.setAlignment(Pos.CENTER);
+			VBox vbox = new VBox(titleText, hbox);
+			vbox.setAlignment(Pos.BASELINE_CENTER);
+			vbox.setSpacing(50);
+			
+			Scene scene = new Scene(vbox, SCENE_WIDTH, SCENE_HEIGHT);
+			stage.setScene(scene);
+			stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	
+	
 	public static void main(String[] args) {
+		try {
+			user = args[0];
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		launch(args);
 	}
 }
--- Battleship/src/application/BattleshipController.java
+++ Battleship/src/application/BattleshipController.java
@@ -4,6 +4,45 @@
 
 package application;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class BattleshipController {
+
+	public BattleshipController(String user) {
+		if (user.toUpperCase().equals("SERVER")) {
+			try {
+				ServerSocket server = new ServerSocket(4000);
+				Socket connection = server.accept();
+				ObjectOutputStream output = new ObjectOutputStream(connection.getOutputStream());
+				ObjectInputStream input = new ObjectInputStream(connection.getInputStream());
+				// Do some IO.
+				connection.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else if (user.toUpperCase().equals("CLIENT")) {
+			try {
+				Socket server = new Socket("localhost", 4000);
+				ObjectOutputStream output = new
+					ObjectOutputStream(server.getOutputStream());
+				ObjectInputStream input = new
+					ObjectInputStream(server.getInputStream());
+				// Do some IO with the server
+				server.close();
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
 
 }
